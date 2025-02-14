5707c761e1d3ddd49b9e8fbf96a01be27260963a
Rekha <rrekha.mmathur@gmail.com>
2025-02-14 12:16:42 +0530

    added browsersync and its implementatio

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
+var SOURCEPATH = {
+    sassSource : 'src/scss/*.scss'
+};
+
+var APPPATH = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
 
 gulp.task('sass', function(){
-    return gulp.src('./src/scss/app.scss')
+    return gulp.src(SOURCEPATH.sassSource)
         .pipe(sass({ outputStyle:'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'));
+        .pipe(gulp.dest(APPPATH.css));
 });
+
+gulp.task('serve', gulp.series( 'sass', function() {
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+        server: {
+            baseDir: APPPATH.root
+        }
+    })
+}));
