f83adb7e67f635b387e4b5d1fc7b783b9ccafa4a
Rekha <rrekha.mmathur@gmail.com>
2025-02-14 10:23:09 +0530

    added first gulp task and run it

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+var sass = require('gulp-sass')(require('sass'));
+
+
+gulp.task('sass', function(){
+    return gulp.src('./src/scss/app.scss')
+        .pipe(sass({ outputStyle:'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./app/css'));
+});
